0e533a3b78d5a0332da1eb3e8608c13c30fa6bc0
production <production@localhost>
2012-09-07 11:22:20 -0500

    1st stab at OOAD with Head First Design Patterns book examples.

---
+++ src/miniducksimulator/Duck.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniducksimulator;
+
+/**
+ *
+ * @author production
+ */
+public abstract class Duck {
+    
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    
+    public Duck(){
+        
+    }
+    
+    public abstract void displauy();
+    
+    public void performFly(){
+        flyBehavior.fly();
+    }  
+    
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+    
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+    
+}
